2293610d275e8f329921c84598ca5ae5cba49a4d
testsnake <testsnake12@gmail.com>
2023-03-21 22:40:10 -0700

    added entry thingy

--- index.js
+++ index.js
@@ -120,9 +120,54 @@ client.once("ready", async client => {
 
 client.on('messageCreate', (message) => {
 	try {
-		if(message.author.bot) return;
-		console.log("Received a message: " + message.content);
+		if (message.author.bot) return;
+		console.log(`[${message.author.username}]: ${message.content}`);
 
+		if (message.channel.id === '1087921223251542088') {
+			let hasMedia = false;
+
+			// Check if the message has any attachments
+			if (message.attachments.size > 0) {
+				hasMedia = true;
+			}
+
+			// Check if the message has any embeds with media
+			message.embeds.forEach((embed) => {
+				if (embed.type === 'image' || embed.type === 'video' || embed.type === 'gifv') {
+					hasMedia = true;
+				}
+			});
+
+			// Check if the message has any links to media files
+			const mediaExtensions = ['.png', '.jpg', '.jpeg', '.gif', '.webp', '.mp4', '.avi', '.mov', '.wmv'];
+			const messageContent = message.content.toLowerCase();
+			for (const extension of mediaExtensions) {
+				if (messageContent.includes(extension)) {
+					hasMedia = true;
+					break;
+				}
+			}
+
+			// Check if the message has a Tenor GIF link
+			if (message.content.includes('tenor.com/view/')) {
+				hasMedia = true;
+			}
+
+
+			if (hasMedia) {
+				console.log(`${message.author.username} sent a message with media in channel ${message.channel.name}.`);
+
+				// Add the role to the user
+				const roleToAdd = message.guild.roles.cache.get('1087921322832699412');
+				if (roleToAdd) {
+					message.member.roles.add(roleToAdd)
+						.then(() => console.log(`Added role ${roleToAdd.name} to ${message.author.username}.`))
+						.catch(console.error);
+				} else {
+					console.error('Role not found.');
+				}
+			}
+		}
 
 
 		// if (rxt(message, /\bass\b/i)) {
