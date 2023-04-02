83bdf417228bacd24a6c6f32b64d5f19481248b1
testsnake <testsnake12@gmail.com>
2023-04-02 16:07:24 -0700

    adjustments

--- commands/help.js
+++ commands/help.js
@@ -20,12 +20,12 @@ module.exports = {
 		const helpEmbed = new EmbedBuilder()
 			.setColor(0x86cecb)
 			.setAuthor({ name: 'Command List', iconURL: 'https://images.gamebanana.com/img/ico/games/6296031c71087.png'})
-			.setDescription("Miku Bot is currently under construction, as a result some commands may not be avalible yet.\nThis bot has been ported from the eden project server, so im still adjusting things. Some commands on this list may not be available. - Testsnake")
+			.setDescription("Current list of commands. If you have any suggestions for commands, please contact testsnake#6663")
 			.addFields(
 				{ name: 'Utility', value: textUtility},
 				{ name: 'Troubleshooting', value: textTroubleshooting},
 				{ name: 'Fun', value: textFun},
-				{ name: 'Temp', value: textTemp}
+				/*{ name: 'Temp', value: textTemp}*/
 			)
 			.setFooter({ text: `${mikuBotVer}`})
 			.setTimestamp();
