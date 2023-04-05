d8b1bfd3615965928abfdb9ca609042a31df17b5
testsnake <testsnake12@gmail.com>
2023-04-04 19:24:41 -0700

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -66,11 +66,11 @@ module.exports = {
                 const row = new ActionRowBuilder()
                     .addComponents(
                         new ButtonBuilder()
-                            .setCustomId(`1087852383477977219_kick_confirm_${member.id}`)
+                            .setCustomId(`Miku Bot#7587_kick_confirm_${member.id}`)
                             .setLabel('Kick')
                             .setStyle('Secondary'),
                         new ButtonBuilder()
-                            .setCustomId(`1087852383477977219_ban_confirm_${member.id}`)
+                            .setCustomId(`Miku Bot#7587_ban_confirm_${member.id}`)
                             .setLabel('Ban')
                             .setStyle('Danger')
                     );
