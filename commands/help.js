9950acfd92fddde2d417963b5ef472be93e1336e
testsnake <testsnake12@gmail.com>
2023-04-05 22:15:51 -0700

    Updated Help Command

--- commands/help.js
+++ commands/help.js
@@ -11,13 +11,17 @@ const filePathF = './text/Fun.txt';
 const textFun = fs.readFileSync(filePathF, 'utf8');
 const filePathTemp = './text/Temp.txt';
 const textTemp = fs.readFileSync(filePathTemp, 'utf8');
+const filePathBooster = './text/Booster.txt';
+const textBooster = fs.readFileSync(filePathBooster, 'utf8')
+const filePathAdmin = './text/Admin.txt';
+const textAdmin = fs.readFileSync(filePathAdmin, 'utf8')
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
 		.setDescription('Gives a list of avalible commands.'),
 	async execute(interaction) {
-		const helpEmbed = new EmbedBuilder()
+		let helpEmbed = new EmbedBuilder()
 			.setColor(0x86cecb)
 			.setAuthor({ name: 'Command List', iconURL: 'https://images.gamebanana.com/img/ico/games/6296031c71087.png'})
 			.setDescription("Current list of commands. If you have any suggestions for commands, please contact testsnake#6663")
@@ -29,6 +33,17 @@ module.exports = {
 			)
 			.setFooter({ text: `${mikuBotVer}`})
 			.setTimestamp();
+		if (interaction.member.roles.cache.has('1092636310142980127')) {
+			helpEmbed.addFields(
+				{ name: 'Booster', value: textBooster}
+			)
+		}
+		if (interaction.member.roles.cache.has('1087782913199833158') || interaction.member.roles.cache.has('1087782822879690772')) {
+			helpEmbed.addFields(
+				{ name: 'Admin', value: textAdmin}
+			)
+		}
+
 		await interaction.reply({ embeds: [helpEmbed] });
 	},
 };
