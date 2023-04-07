12478339c5d4c933ca8df0cca5b90ec949c215b2
testsnake <testsnake12@gmail.com>
2023-04-07 13:30:14 -0700

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -6,48 +6,53 @@ module.exports = {
     .setDescription('Deletes the last X messages in the channel.')
     .addIntegerOption(option => option.setName('count').setDescription('The number of messages to delete (max 100).').setRequired(true)),
   async execute(interaction) {
-    // Check if the command user has permission to manage messages in the channel.
-    if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
-      return await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
-    }
-
-    const count = interaction.options.getInteger('count');
-
-    if (count <= 0 || count > 100) {
-      return await interaction.reply({ content: 'Invalid message count. Please enter a number between 1 and 100.', ephemeral: true });
-    }
-
-    const channel = interaction.channel;
-
     try {
-      const messages = await channel.messages.fetch({ limit: count  }); // Fetch the messages to be deleted.
-      await channel.bulkDelete(messages); // Delete the messages.
+      // Check if the command user has permission to manage messages in the channel.
+      if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
+        return await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+      }
 
-      const purgeEmbed = {
-        color: 0x00ff00,
-        title: 'Purged Messages',
-        description: `Deleted ${count} messages.`,
-        timestamp: new Date(),
-      };
+      const count = interaction.options.getInteger('count');
 
-      // Send an embed message to the current channel indicating the number of messages deleted.
-      const replyMessage = await interaction.reply({ embeds: [purgeEmbed], fetchReply: true });
-      setTimeout(() => {
-        replyMessage.delete().catch(console.error);
-      }, 5000); // Delete the reply message after 5 seconds.
+      if (count <= 0 || count > 100) {
+        return await interaction.reply({ content: 'Invalid message count. Please enter a number between 1 and 100.', ephemeral: true });
+      }
 
-      // Send an embed message to the logging channel indicating the number of messages deleted.
-      const loggingChannel = interaction.client.channels.cache.get('1087810388936114316');
-      if (loggingChannel) {
-        const loggingEmbed = {
-          ...purgeEmbed,
-          description: `Deleted ${count} messages in ${channel.toString()}.`,
+      const channel = interaction.channel;
+
+      try {
+        const messages = await channel.messages.fetch({ limit: count  }); // Fetch the messages to be deleted.
+        await channel.bulkDelete(messages); // Delete the messages.
+
+        const purgeEmbed = {
+          color: 0x00ff00,
+          title: 'Purged Messages',
+          description: `Deleted ${count} messages.`,
+          timestamp: new Date(),
         };
-        await loggingChannel.send({ embeds: [loggingEmbed] });
+
+        // Send an embed message to the current channel indicating the number of messages deleted.
+        const replyMessage = await interaction.reply({ embeds: [purgeEmbed], fetchReply: true });
+        setTimeout(() => {
+          replyMessage.delete().catch(console.error);
+        }, 5000); // Delete the reply message after 5 seconds.
+
+        // Send an embed message to the logging channel indicating the number of messages deleted.
+        const loggingChannel = interaction.client.channels.cache.get('1087810388936114316');
+        if (loggingChannel) {
+          const loggingEmbed = {
+            ...purgeEmbed,
+            description: `Deleted ${count} messages in ${channel.toString()}.`,
+          };
+          await loggingChannel.send({ embeds: [loggingEmbed] });
+        }
+      } catch (error) {
+        console.error(error);
+        return await interaction.reply({ content: 'There was an error purging messages. Please try again.', ephemeral: true });
       }
     } catch (error) {
-      console.error(error);
-      return await interaction.reply({ content: 'There was an error purging messages. Please try again.', ephemeral: true });
+        console.error(error);
+        return await interaction.reply({ content: 'There was an error purging messages. Please try again.', ephemeral: true });
     }
   },
 };
