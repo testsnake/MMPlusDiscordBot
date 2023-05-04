961854c616b71cdb6f2b9b181bffedb20103bcbc
testsnake <testsnake12@gmail.com>
2023-05-03 21:34:50 -0700

    Added atmb to fuck with people

--- commands/atmb.js
+++ commands/atmb.js
@@ -1,22 +1,24 @@
-const {slashCommandBuilder, PermissionFlagsBits } = require('@discordjs/builders');
+const {SlashCommandBuilder, PermissionFlagsBits } = require('@discordjs/builders');
 
 module.exports = {
-    data: slashCommandBuilder()
+    data: new SlashCommandBuilder()
         .setName('atmb')
         .setDescription('Act as miku bot')
-        .setDefaultPermission(0)
+        .setDefaultPermission(false)
         .addChannelOption(option => option.setName('channel').setDescription('Channel to send message to').setRequired(true))
         .addStringOption(option => option.setName('message').setDescription('Message to send').setRequired(true)),
+
     async execute(interaction) {
         const channel = interaction.options.getChannel('channel');
         const message = interaction.options.getString('message');
-        if (!channel.isText()) {
+        if (!channel.isTextBased()) {
             return interaction.reply({ content: `Error: Channel must be a text channel.`, ephemeral: true });
         }
         await channel.sendTyping();
+        await interaction.reply({ content: `Message sending.`, ephemeral: true });
         await new Promise(resolve => setTimeout(resolve, 4000));
         await channel.send(message);
-        await interaction.reply({ content: `Message sent.`, ephemeral: true });
+        await interaction.editReply({ content: `Message sent.`, ephemeral: true });
     }
 
 }
