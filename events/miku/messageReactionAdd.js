b8d38dd322c2935b0b92c4c2a3302f494ca3415e
testsnake <testsnake12@gmail.com>
2023-06-12 23:02:13 -0700

    Update messageReactionAdd.js

--- events/miku/messageReactionAdd.js
+++ events/miku/messageReactionAdd.js
@@ -7,7 +7,7 @@ module.exports = {
     name: 'messageReactionAdd',
     async execute(reaction, message, client) {
         log.debug(`[REACTION] ${reaction.emoji.name} added to ${reaction.message.id} in ${reaction.message.channel.name} in ${reaction.message.guild.name}`)
-        if (message.channel.id === config.starboardChannelID) return;
+        if (message.author.bot) return;
 
         if (reaction.emoji.name === '⭐' && reaction.count === 4) {
             await sendStarboardEmbed(reaction.message.id, reaction.message.channel);
