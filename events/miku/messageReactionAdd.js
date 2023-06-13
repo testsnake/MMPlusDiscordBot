d820fb0e8c7db12f355a923ffffb401e040d9733
testsnake <testsnake12@gmail.com>
2023-06-12 22:59:18 -0700

    Update messageReactionAdd.js

--- events/miku/messageReactionAdd.js
+++ events/miku/messageReactionAdd.js
@@ -1,10 +1,12 @@
 
 const { sendStarboardEmbed } = require('../../utils.js');
 const config = require('../../config.json');
+const log = require('../../logger.js');
 
 module.exports = {
     name: 'messageReactionAdd',
     async execute(reaction, message, client) {
+        log.debug(`[REACTION] ${reaction.emoji.name} added to ${reaction.message.id} in ${reaction.message.channel.name} in ${reaction.message.guild.name}`)
         if (message.channel.id === config.starboardChannelID) return;
 
         if (reaction.emoji.name === '⭐' && reaction.count === 4) {
